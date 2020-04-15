5893f7eff1511919ff1c6ae3be25c4799594752e
Rikam <36474843+RikamCZ@users.noreply.github.com>
2020-04-15 15:55:42 +0530

    Project checkin

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace My_First_WPF_App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            BootStrapper bootStrapper = new BootStrapper();
+            bootStrapper.Run();
+
+        }
+    }
+}
